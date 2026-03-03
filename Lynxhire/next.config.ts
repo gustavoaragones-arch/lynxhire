2a483a9a28b82791fcb71af34c5a50f230ea7ccf
Gustavo Aragones-Malmborg <gustavoaragones@gmail.com>
2026-03-03 15:19:00 -0700

    Remove eslint config from next.config.ts

--- Lynxhire/next.config.ts
+++ Lynxhire/next.config.ts
@@ -29,9 +29,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
 };
 
 export default withContentCollections(nextConfig);
